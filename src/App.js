e2f21d6356818b2781a9a2241790c7ff01614d09
sarulathaannadurai@gmail.com <you@example.com>
2020-05-30 21:08:36 +0530

    commited sucessfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/layouts/Navbar";
 import './App.css';
-
+import {BrowserRouter, Route, Switch } from "react-router-dom";
+import Index from "./components/layouts/Index";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
